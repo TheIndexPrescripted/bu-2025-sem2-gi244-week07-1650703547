0b6630fc7c9f42e28361cd014d82d10d5758e7db
TheIndexPrescripted <akkanat.wong@bumail.net>
2026-02-27 16:44:22 +0700

    Wip1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private InputAction jumpAction;
 
+    public bool IsGameOver = false;
+
+    private bool IsOnGround = false;
+
     void Awake()
     {
+        IsGameOver = false;
         rb = GetComponent<Rigidbody>();
         jumpAction = InputSystem.actions.FindAction("Jump");
     }
@@ -17,15 +22,33 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Physics.gravity *= gravityMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (jumpAction.triggered)
+        if (jumpAction.triggered && IsOnGround == true && IsGameOver == false)
         {
-
+            rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+            IsOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Hit " + collision.gameObject.name);
+        //if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("GAME OVER");
+            IsGameOver = true;
+        }
+
+
     }
 }
